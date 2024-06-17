311bdd934247335e98be7f90710cce70fe2c044e
Кияница Максим <KiyanitsaMA@tomskasu.ru>
2024-06-17 15:31:33 +0700

    Исправление зависимостей

--- src/app/modules/sidenav/sidenav.module.ts
+++ src/app/modules/sidenav/sidenav.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
-import { MenuComponent } from "./menu/menu.component";
-import { MeasurementComponent } from "./measurement/measurement.component";
-import { DrawComponent } from "./draw/draw.component";
-import { CoordinatesComponent } from "./coordinates/coordinates.component";
-import { MeasurementModule } from "./measurement/measurement.module";
-import { MenuModule } from "./menu/menu.module";
-import { DrawModule } from "./draw/draw.module";
-import { CoordinatesModule } from "./coordinates/coordinates.module";
+import { MenuComponent } from "./modules/menu/menu.component";
+import { MeasurementComponent } from "./modules/measurement/measurement.component";
+import { DrawComponent } from "./modules/draw/draw.component";
+import { CoordinatesComponent } from "./modules/coordinates/coordinates.component";
+import { MeasurementModule } from "./modules/measurement/measurement.module";
+import { MenuModule } from "./modules/menu/menu.module";
+import { DrawModule } from "./modules/draw/draw.module";
+import { CoordinatesModule } from "./modules/coordinates/coordinates.module";
 
 @NgModule({
 	declarations: [],
@@ -18,7 +18,6 @@ import { CoordinatesModule } from "./coordinates/coordinates.module";
 		MenuModule,
 		DrawModule,
 		CoordinatesModule,
-		
 	],
 	exports: [
 		MenuComponent,
